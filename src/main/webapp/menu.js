33f05b40d4bb4ae410dad5053410fbc9e69d825f
Diegoe de Oliveira Galdino <dgaldino2007@gmail.com>
2026-05-06 18:04:17 -0300

    Atualização

---
+++ src/main/webapp/menu.js
@@ -0,0 +1,11 @@
+$('.seguros').click(function(){
+    $('.submenuSeguros').toggleClass('mostra');
+});
+
+$('.sobre').click(function(){
+    $('.submenuSobre').toggleClass('mostra');
+});
+
+$('.btnAbre').click(function(){
+    $('#menuprincipal').toggleClass('mostra');
+});
